3d4506f386541d26a4e398130ddd57a8b75becfb
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-05-28 23:38:32 +0530

    deploy fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://your-frontend-app-name.onrender.com" //"http://localhost:3001/api/v1",
 });
 
 API.interceptors.request.use((req) => {
